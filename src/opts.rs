bc208302a29cfdb64f22a1297a12971f106b3cf6
ohmycloud <ohmycloudy@gmail.com>
2025-03-02 21:56:17 +0800

    refactor

---
+++ src/opts.rs
@@ -0,0 +1,91 @@
+use std::{fs, path::Path};
+
+use clap::Parser;
+use csv::{Reader, ReaderBuilder, StringRecord};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct Player {
+    pub name: String,
+    pub position: String,
+    #[serde(rename = "DOB")]
+    pub dob: String,
+    pub nationality: String,
+    #[serde(rename = "Kit Number")]
+    pub kit_number: u8,
+}
+
+#[derive(Debug, Parser, Clone)]
+pub struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input_file)]
+    pub input: String,
+    #[arg(short, long, default_value = "output.json")]
+    pub output: String,
+    #[arg(short, long, default_value_t = ',')]
+    pub delimiter: char,
+    #[arg(long, default_value_t = true)]
+    pub header: bool,
+}
+
+#[derive(Debug, Parser, Clone)]
+pub enum SubCommand {
+    #[command(name = "csv", about = "Show CSV, or convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, about, long_about = None)]
+pub struct Opts {
+    #[command(subcommand)]
+    pub cmd: SubCommand,
+}
+
+fn verify_input_file(input: &str) -> Result<String, &'static str> {
+    if Path::new(input).exists() {
+        Ok(input.into())
+    } else {
+        Err("File does not exist.")
+    }
+}
+
+pub fn read_with_csv_builder(opts: CsvOpts) -> anyhow::Result<Vec<StringRecord>> {
+    let mut rdr = ReaderBuilder::new()
+        .delimiter(opts.delimiter as u8)
+        .has_headers(opts.header)
+        .from_path(opts.input)
+        .expect("Failed to open CSV file");
+
+    let records = rdr.records().collect::<Result<Vec<StringRecord>, _>>()?;
+
+    Ok(records)
+}
+
+pub fn deserialize_csv(opts: CsvOpts) -> anyhow::Result<Vec<Player>> {
+    let mut rdr = Reader::from_path(opts.input)?;
+
+    let records = rdr
+        .deserialize::<Player>()
+        .collect::<Result<Vec<Player>, _>>()?;
+
+    Ok(records)
+}
+
+pub fn serialize_record_to_json(records: Vec<StringRecord>, opts: CsvOpts) -> anyhow::Result<()> {
+    let json_data: Vec<Vec<String>> = records
+        .into_iter()
+        .map(|record| record.into_iter().map(String::from).collect())
+        .collect();
+
+    let json_string = serde_json::to_string_pretty(&json_data)?;
+    fs::write(format!("record_{}", opts.output), json_string)?;
+
+    Ok(())
+}
+
+pub fn serialize_player_to_json(records: Vec<Player>, opts: CsvOpts) -> anyhow::Result<()> {
+    let json_string = serde_json::to_string_pretty(&records)?;
+    fs::write(opts.output, json_string)?;
+
+    Ok(())
+}
