43c37bd219e398cb929ea66be54ea48c62c4d1fb
ohmycloud <ohmycloudy@gmail.com>
2025-03-02 22:39:41 +0800

    add csv header

--- src/process.rs
+++ src/process.rs
@@ -1,16 +1,22 @@
 use csv::{Reader, ReaderBuilder, StringRecord};
+use serde_json::Value;
 use std::fs;
 
 use crate::{CsvOpts, Player};
 
-pub fn read_with_csv_builder(opts: CsvOpts) -> anyhow::Result<Vec<StringRecord>> {
+pub fn read_with_csv_builder(opts: CsvOpts) -> anyhow::Result<Vec<Value>> {
     let mut rdr = ReaderBuilder::new()
         .delimiter(opts.delimiter as u8)
         .has_headers(opts.header)
         .from_path(opts.input)
         .expect("Failed to open CSV file");
 
+    let headers = rdr.headers()?.clone();
     let records = rdr.records().collect::<Result<Vec<StringRecord>, _>>()?;
+    let records = records
+        .iter()
+        .map(|record| headers.iter().zip(record.iter()).collect::<Value>())
+        .collect::<Vec<Value>>();
 
     Ok(records)
 }
@@ -25,13 +31,8 @@ pub fn deserialize_csv(opts: CsvOpts) -> anyhow::Result<Vec<Player>> {
     Ok(records)
 }
 
-pub fn serialize_record_to_json(records: Vec<StringRecord>, opts: CsvOpts) -> anyhow::Result<()> {
-    let json_data: Vec<Vec<String>> = records
-        .into_iter()
-        .map(|record| record.into_iter().map(String::from).collect())
-        .collect();
-
-    let json_string = serde_json::to_string_pretty(&json_data)?;
+pub fn serialize_record_to_json(records: Vec<Value>, opts: CsvOpts) -> anyhow::Result<()> {
+    let json_string = serde_json::to_string_pretty(&records)?;
     fs::write(format!("record_{}", opts.output), json_string)?;
 
     Ok(())
