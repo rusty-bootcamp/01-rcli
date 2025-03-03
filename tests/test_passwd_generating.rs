af4a6f0a7ebe651f6a7750c3d151ed9223c6ce5c
ohmycloud <ohmycloudy@gmail.com>
2025-03-03 20:14:49 +0800

    add test for password generating

---
+++ tests/test_passwd_generating.rs
@@ -0,0 +1,28 @@
+use anyhow::Ok;
+use rcli::{GenPassOpts, process_passwd};
+
+#[test]
+fn test_process_passwd_generating() -> anyhow::Result<()> {
+    let opts = GenPassOpts {
+        length: 16,
+        uppercase: false,
+        lowercase: true,
+        number: true,
+        symbol: true,
+    };
+
+    let passwd = process_passwd(&opts)?;
+    assert_eq!(passwd.len(), 16);
+
+    let has_uppercase = passwd.chars().any(|c| c.is_ascii_uppercase());
+    let has_lowercase = passwd.chars().any(|c| c.is_ascii_lowercase());
+    let has_number = passwd.chars().any(|c| c.is_ascii_digit());
+    let has_symbol = passwd.chars().any(|c| "!@#$%^&*_".contains(c));
+
+    assert!(!has_uppercase);
+    assert!(has_lowercase);
+    assert!(has_number);
+    assert!(has_symbol);
+
+    Ok(())
+}
