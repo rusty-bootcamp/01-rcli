233dd2bf1814c2ac78b631545302a0f9b250d183
ohmycloud <ohmycloudy@gmail.com>
2025-03-08 12:47:43 +0800

    refine the main function

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,7 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     let opts = Opts::parse();
 
-    match opts.cmd {
-        SubCommand::Csv(opts) => opts.execute().await?,
-        SubCommand::GenPass(opts) => opts.execute().await?,
-        SubCommand::Base64(opts) => opts.execute().await?,
-        SubCommand::Crypto(opts) => opts.execute().await?,
-        SubCommand::Http(opts) => opts.execute().await?,
-    }
+    opts.cmd.execute().await?;
 
     Ok(())
 }
